3b701fc9c9ba5b848a9f5d032c7329e4148c4cdb
arunkumar6201 <arunkumar620159@gmail.com>
2026-05-08 10:28:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, sans-serif;
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+body {
+  background: #eef2f7;
+}
+
+button {
+  cursor: pointer;
+}
